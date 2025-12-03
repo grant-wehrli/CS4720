9aea207d6d50efd784cf39746051710fe25f9580
Scout1218 <48729865+Scout1218@users.noreply.github.com>
2025-12-03 01:49:40 +0000

    changed name to sizes from weights to match pseudocode better

--- knapsack/knapsack.py
+++ knapsack/knapsack.py
@@ -45,7 +45,7 @@ for i = 1 to n:
 
 optimal value - A[n][W]
 '''
-def knapsack(capacity, values, weights):
+def knapsack(capacity, values, sizes):
     n = len(values)
     A = [[0 for _ in range(capacity + 1)] for _ in range(n + 1)]
 
@@ -54,16 +54,50 @@ def knapsack(capacity, values, weights):
     
     for i in range(1, n + 1):
         for x in range(capacity + 1):
-            weight = weights[i - 1]
+            size = sizes[i - 1]
             value = values[i - 1]
-            if weight > x:
+            if size > x:
                 A[i][x] = A[i-1][x]
             else:
                 skip = A[i-1][x]
-                take = A[i-1][x - weight] + value
+                take = A[i-1][x - size] + value
                 A[i][x] = max(skip, take)
 
     return A[n][capacity]
 
-capacity, values, weights = read_file("problem16.7test.txt")
-print(knapsack(capacity, values, weights))
+capacity, values, sizes = read_file("problem16.7test.txt")
+print(knapsack(capacity, values, sizes))
+
+'''reconstruct knapsack contents pseudocode
+   s[1 to n] init to false
+
+   input: the array A computed by the Knapsack
+algorithm with item values v1, v2, . . . , vn, item sizes
+s1, s2, . . . , sn, and knapsack capacity C.
+Output: an optimal knapsack solution.
+S := False ; // items in an optimal solution
+c := capacity // remaining capacity
+for i := n downto 1 do
+if s_i < c and A[i - 1][c - s_i] + v_i  A[i - 1][c] then
+S := S union {i} // Case 2 wins, include i
+c := c - s_i // reserve space for it
+// else skip i, capacity stays the same
+return S
+
+'''
+def reconstruct(A, values, sizes,capacity):
+    n = len(values)
+    # false arr S
+    S = [False] * n
+    c = capacity
+
+    for i in range(n, 0, -1):
+        v_i = values[i - 1]
+        s_i = sizes[i - 1]
+        
+        if s_i <= c and A[i - 1][c - s_i] + v_i >= A[i - 1][c]:
+            S[i - 1] = True
+            x = x - s_i
+
+    return S
+
